27c5aacee5fb65117eb7c9b0ea4a31be6df45910
dbrtvrsky <kallelr@outlook.com>
2020-12-10 15:17:23 -0300

    commiting what's been done so far

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  tralingComma: 'All',
+  arrowParens: 'avoid',
+}
---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'f47aa56d803a3537b6ae403b619d78dd',
+    expiresIn: '1d',
+  },
+};
