892fe908a0dbd63a6f0c174ac36aa4e5203b7ad3
kallelrm <kallelr@outlook.com>
2021-03-03 00:45:33 -0300

    Mudança de configuração do orm e criação de testes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,8 +4,8 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
-import AppError from '../../errors/AppError';
-import uploadConfig from '../../../config/upload';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
--- src/shared/infra/typeorm/migrations/1603425951300-createAppointments.ts
+++ src/shared/infra/typeorm/migrations/1603425951300-createAppointments.ts
@@ -3,6 +3,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 export default class createAppointments1603425951300
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'appointments',
